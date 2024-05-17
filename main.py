2679ad4933affacde26cccf34b8a10438eed4f14
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-05-17 16:15:01 -0500

    parser usage change

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 
 from Minizinc import Minizinc
 from tools.File_selector import File_selector
+from QueensProblem.QueensProblemParser import Parser
 
 mzInstance = Minizinc()
 mzInstance.set_model(File_selector.select())
-data = mzInstance.parse_input_file_to_dzn(File_selector.select())
+data = Parser.parse_input_file_to_dzn(File_selector.select())
 mzInstance.set_data(data)
 mzInstance.set_solver("Gecode")
 result = mzInstance.solve()
 print(result)
+
+
+if __name__ == "__main__":
+    from PUICAProblem.PUICAProblemParser import Parser
+    Parser.test_parse_input_file_to_dzn()
